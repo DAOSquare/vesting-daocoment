94743a0cf57d2108eb5c15b09d071445546ff845
Harvey <31661937+HarveyKeitel@users.noreply.github.com>
2022-04-13 13:53:25 +0800

    Update vuepress.config.js

--- vuepress.config.js
+++ vuepress.config.js
@@ -1,30 +1,60 @@
+
 module.exports = {
   head: [["link", { rel: "icon", href: "/images/logo.png" }]],
   locales: {
     // The key is the path for the locale to be nested under.
     // As a special case, the default locale can use '/' as its path.
     "/": {
-      lang: " DAOSquare Portfolios",
-      title: "DAOSquare Portfolios",
-      description: "DAOSquare's Portfolios",
+      lang: "DAOSquare",
+      title: "DAOSquare",
+      description: "DAOSquare's Vesting",
     },
   },
-  plugins: [["@vuepress/plugin-search"]],
+  // plugins: [["@vuepress/plugin-search"]],
   themeConfig: {
-    logo: "/images/logoMadbell.svg",
+    logo: "/images/DAOSquareLogo_Round.svg",
     sidebar: [
       {
-        text: "IQ Protocol",
+        text: "Vesting",
         link: "/",
         children: [
           {
-            text: "Vesting Period",
-            link: "/guide/iq.md",
+            text: "About Vesting",
+            link: "/guide/vp.md",
+          },
+          {
+            text: "About Network and Wallet",
+            link: "/guide/Network_Wallet.md",
+          },
+          {
+            text: "How To Claim Token",
+            link: "/guide/Claim_Token.md",
+          },
+          {
+            text: "Explanation Of Terms",
+            link: "/guide/Explanation_of_terms.md",
           },
         ],
       },
-
-    ]
+      {
+        text: "Announcement",
+        children: [
+          {
+            text: "Vesting Test Experience Event",
+            link: "/guide/20220412.md",
+          },
+        ],
+      },
+      {
+        text: "About Version",
+        children: [
+          {
+            text: "Version Record",
+            link: "/guide/Version_Record.md",
+          },
+        ],
+      },
+    ],
   }
 
 
